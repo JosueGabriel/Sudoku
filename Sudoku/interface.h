602a170bf636c2c3339fce7f83d1b90ae1237e0c
Josué <josueqabriel24@gmail.com>
2020-02-18 15:19:13 -0300

    fim

--- Sudoku/interface.h
+++ Sudoku/interface.h
@@ -6,14 +6,21 @@
 
 void displaySudoku(){
     std::cout << "  || A | B | C | D | E | F | G | H | I |" << std::endl;
-    for (auto var1 = 0; var1 < Size; var1++) {
+    for (size_t var1 = 0; var1 < Size; var1++) {
         std::cout << var1 + 1 << " || ";
-        for (auto var = 0; var < Size; var++){
-            std::cout << Matrix[var1][var] << " | ";
+        for (size_t var = 0; var < Size; var++){
+            if(Matrix[var1][var] == 0)
+                std::cout << "  | ";
+            else
+                std::cout << Matrix[var1][var] << " | ";
         }
         std::cout << std::endl;
     }
     std::cout << std::endl;
+}
+
+void interfaceMenu(){
+    std::cout<<" 1 - logicCut = TRUE" << std::endl << " 2 - logicCut = FALSE" << std::endl;
 }
 
 #endif // INTERFACE_H
--- Sudoku/main.cpp
+++ Sudoku/main.cpp
@@ -1,11 +1,39 @@
 #include "sudoku.h"
 #include "interface.h"
+#include <time.h>
 
 int main()
 {
-    configureAll();
+    estruturaStatic(99);
+    geradorSudoku(60);
     displaySudoku();
+    configureAll();
     solver();
     displaySudoku();
+
+    /*
+    interfaceMenu();
+    int L;
+    std::cin >> L;
+    if(L == 2){
+        logicCut = !logicCut;
+    }
+    system("cls");
+
+    clock_t end,start;
+
+    start = clock();
+
+    configureAll();
+    displaySudoku();
+
+    solver();
+    displaySudoku();
+    std::cout<< contador <<" - " << logicCut << std::endl;
+
+    end = clock();
+
+    printf("Tempo gasto: %4.0f ms\n\n",1000*(double)(end-start)/(double)(CLOCKS_PER_SEC));
+    */
     return 0;
 }
